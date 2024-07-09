feb1efefafe27a1fd8c67478351969bfc9a5c7eb
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2024-07-09 11:42:45 +0200

    clean-up

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,27 +1,5 @@
 use clap::Parser;
 
-/*
-    /// Do not demangle function names.
-    #[arg(long, default_value_t = false)]
-    pub no_demangle: bool,
-
-    /// List all functions.
-    #[arg(short, long, default_value_t = false)]
-    pub list_all_functions: bool,
-
-    /// List local functions.
-    #[arg(long, default_value_t = false)]
-    pub list_local_functions: bool,
-
-    /// List imports.
-    #[arg(long, default_value_t = false)]
-    pub list_imports: bool,
-
-    /// List start function.
-    #[arg(long, default_value_t = false)]
-    pub list_start_function: bool,
-*/
-
 #[derive(Parser, Debug)]
 #[command(version, about=format!("Wasi dependency removal V{}", env!("CARGO_PKG_VERSION")), long_about = None)]
 pub struct Wasm2icArgs {
